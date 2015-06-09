74ad5204a9b1bc788cba368da5e62c1c904b7ec4
sle-guil <sle-guil@student.42.fr>
2015-06-09 13:26:27 +0200

    FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU

--- ft_strcdup.c
+++ ft_strcdup.c
@@ -6,7 +6,7 @@
 /*   By: sle-guil <sle-guil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/14 18:43:44 by sle-guil          #+#    #+#             */
-/*   Updated: 2014/12/30 17:35:37 by sle-guil         ###   ########.fr       */
+/*   Updated: 2015/06/09 13:26:06 by sle-guil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strcdup(const char *src, char c)
 	if (n)
 	{
 		new = ft_strnew(sizeof(char) * (n + 1));
-		strncat(new, src, n);
+		ft_strncat(new, src, n);
 	}
 	else
 		new = ft_strdup(src);
